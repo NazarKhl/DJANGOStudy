02bf1ebabf6830e317a663084fe590b9198345e5
Nazar Khlivnyi <135509585+NazarKhl@users.noreply.github.com>
2025-02-06 11:52:55 +0100

    Working on views, models, urls

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -8,4 +8,9 @@ def index(request):
         'name': 'Name',
         'age': 21,
     }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
+
+def counter(request):
+    text = request.GET['text']
+    amount = len(text.split())
+    return render(request, 'counter.html', {'amount': amount})
